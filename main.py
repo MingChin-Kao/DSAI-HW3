4dd98353a8be2f9238c4ec67a04bb91cd7e09657
MingChin-Kao <54504033+MingChin-Kao@users.noreply.github.com>
2022-05-14 23:47:07 +0800

    template

---
+++ main.py
@@ -0,0 +1,40 @@
+
+# You should not modify this part.
+import pandas as pd
+import datetime
+
+def config():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--consumption", default="./sample_data/consumption.csv", help="input the consumption data path")
+    parser.add_argument("--generation", default="./sample_data/generation.csv", help="input the generation data path")
+    parser.add_argument("--bidresult", default="./sample_data/bidresult.csv", help="input the bids result path")
+    parser.add_argument("--output", default="output.csv", help="output the bids path")
+
+    return parser.parse_args()
+
+
+def output(path, data):
+
+    df = pd.DataFrame(data, columns=["time", "action", "target_price", "target_volume"])
+    df.to_csv(path, index=False)
+
+    return
+
+
+if __name__ == "__main__":
+    args = config()
+
+    data = pd.read_csv(args.generation)
+    last_day = data["time"][data.shape[0]-1]
+    last_day_time = pd.to_datetime(last_day, format="%Y-%m-%d %H:%M:%S")
+
+    predict_date = last_day_time + datetime.timedelta(hours=1)
+    print("predict date is ", predict_date)
+    output_date = datetime.datetime.strftime(predict_date,'%Y-%m-%d %H:%M:%S')
+    data = [[output_date, "buy", 2, 5]]
+
+    # data = [["2018-01-01 00:00:00", "buy", 2.5, 3],
+    #         ["2018-01-01 01:00:00", "sell", 3, 5]]
+    output(args.output, data)
